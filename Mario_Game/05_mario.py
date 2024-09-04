fa69f2c955190b57c85aecac8f88f544f082552a
ToniEngine <antoineorbot@gmail.com>
2024-09-04 16:22:12 +0100

    modified 05_mario.py

--- Mario_Game/05_mario.py
+++ Mario_Game/05_mario.py
@@ -2,8 +2,9 @@ def main():
     print_square(3)
 
 def print_square(size):
-   #For each row in square
     for i in range(size):
-        print("#"* size)
-        
+        print_row(size)
+
+def print_row(width):
+    print("#"* width)        
 main()
