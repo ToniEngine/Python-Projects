eab2d0b3752fd3dc97b665b13e92f83d60b631e2
ToniEngine <antoineorbot@gmail.com>
2024-09-05 18:04:44 +0100

    New Commit

---
+++ ASCII_CALCULATOR/ascii_cal.py
@@ -0,0 +1,12 @@
+
+user_input = input("Enter your key ")
+converted_input = ord(user_input)
+
+if user_input.isalpha():
+        print(f"Your ASCII Value for, {user_input}, is, {converted_input}")
+elif():
+    print(f"Your ASCII Value for, {user_input}, is, {converted_input}")
+
+else:
+      print("ASCII Value not found")
+
