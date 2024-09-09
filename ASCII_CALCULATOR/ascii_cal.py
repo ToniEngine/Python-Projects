865b0f0a076ddc862769c35b8a659278969314a1
ToniEngine <antoineorbot@gmail.com>
2024-09-09 18:24:17 +0100

    Edited the application

--- ASCII_CALCULATOR/ascii_cal.py
+++ ASCII_CALCULATOR/ascii_cal.py
@@ -1,12 +1,23 @@
 
 user_input = input("Enter your key ")
-converted_input = ord(user_input)
 
-if user_input.isalpha():
-        print(f"Your ASCII Value for, {user_input}, is, {converted_input}")
-elif():
-    print(f"Your ASCII Value for, {user_input}, is, {converted_input}")
+# converted_input = ord(user_input)
+# decrypted_input = chr(user_input)
 
-else:
-      print("ASCII Value not found")
+# if user_input.isalpha():
+#         print(f"Your ASCII Value for, {user_input}, is, {converted_input}")
+# elif():
+#     print(f"Your ASCII Value for, {user_input}, is, {converted_input}")
+
+# else:
+#       print("ASCII Value not found")
+
+def ascii_symbol():
+    
+      user_input_symbol = input("Enter Symbol ")
+      ascii_symbol = ord(user_input_symbol)
+
+      print(ascii_symbol)
+
+ascii_symbol()
 
