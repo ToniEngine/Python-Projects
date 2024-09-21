1a1b5a7296e64469ce7179f34450bffd07cde505
ToniEngine <antoineorbot@gmail.com>
2024-09-21 06:46:18 +0100

    Added changes to the program

--- Application/07_Luhn_Algorithm/luhn_algo.py
+++ Application/07_Luhn_Algorithm/luhn_algo.py
@@ -1,11 +1,18 @@
 
 def verify_card_number(card_number):
     sum_of_odd_digits = 0
-    card_number_reversed = card_number[-1:-5:-1]
+    card_number_reversed = card_number[::-1]
+    odd_digits = card_number_reversed[::2]
     print(card_number_reversed)
+    print(odd_digits)
+    for digit in odd_digits:
+        sum_of_odd_digits += int(digit)
+        sum_of_even_digits = 0
+        print(digit)
 
 def main():
     card_number = '4111-1111-4555-1142'
+    print(card_number)
     card_translation =str.maketrans({"-": "", " ": ""})
     translated_card_number = card_number.translate(card_translation)
     print(translated_card_number)
---
+++ Application/08_Coffee_Machine/main.py
@@ -0,0 +1,29 @@
+MENU ={
+     "espresso":{"ingredients":{"water":50, "coffee": 18,}, "cost":1.5,},
+     "latte":{"ingredients":{"water":200, "milk": 150, "coffee": 24,}, "cost":2.5,},
+     "cappuccino":{"ingredients":{"water":250,"milk": 100, "coffee": 24,}, "cost":3.0,}
+
+}
+
+profit = 0
+resources={
+     'water': 300,
+     'milk': 200,
+     'coffee': 100,
+}
+
+is_on = True
+
+
+while is_on:
+      choice = input("What would you like? (espresso/latte/cappuccino) ")
+      if choice == "off":
+            is_on = False
+      elif choice == "report":
+          print(f"Water:{resources['water']}ml") 
+          print(f"Milk:{resources['milk']}ml") 
+          print(f"Coffee: {resources['coffee']}g") 
+          print(f"Money: ${profit} ") 
+        else:
+           drink = MENU[choice]
+             
