cf8f030932674bda20a7693accc6aaaad485ce93
ToniEngine <antoineorbot@gmail.com>
2024-09-22 14:25:33 +0100

    added the dictionary learning

---
+++ Python_Learning/07_dictionaries/02_dictionary.py
@@ -0,0 +1,6 @@
+'''A dictionary is another built-in data type in Python. 
+A dictionary is a collection of data in the form of key-value pairs. 
+Dictionaries are defined with curly braces ({}) and they contain key-value pairs separated by commas. 
+Each key is followed by a colon (:) and the value:'''
+
+{'amount': 50.0, 'category': 'Food'}
