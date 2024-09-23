fe822d0e563171f4fdf2d90de9e490bab89a2b58
ToniEngine <antoineorbot@gmail.com>
2024-09-23 11:01:14 +0100

    updated the variable directory

---
+++ Python_Learning/01_variables/04_math_functions.py
@@ -0,0 +1,32 @@
+import math # We Imported the math module in python
+
+print(math.pi) # It Prints out the value of pi
+
+num = 3.14
+print(round(num)) # The round() function is an in-built python function that rounds to the nearest whole integer
+print(math.ceil(num)) # The ceil() function of the math class rounds the number up to the nearest whole integer
+print(math.floor(num)) # The floor() method of the math clas rounds a number down to the nearest whole integer
+num2 = -3.14
+print(abs(num2))   # Gives the absolute value of a number("How far a no. is away from zero")
+
+# The pow() function rasie a base number to a power
+print(pow(num, 2)) # You pass it two arguments, a base and a power
+
+# The sqrt() function gives the square root of a number
+print(math.sqrt(num))
+print(math.sqrt(100))
+print(math.pow(8,2))
+print(math.sqrt(64))
+
+
+# The max() function finds the largest of a varying amount of values
+x =1
+y=2
+z=3
+print(max(x,y,z))
+
+
+# The min() finds the lowest
+print(min(x,y,z))
+
+
