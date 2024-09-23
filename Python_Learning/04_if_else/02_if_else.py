59b2ff839ab764ec35803ca8af801043a6a31d29
ToniEngine <antoineorbot@gmail.com>
2024-09-23 11:50:37 +0100

    updated the if/else directory

---
+++ Python_Learning/04_if_else/02_if_else.py
@@ -0,0 +1,15 @@
+# if statement = a block of code that will execute if it's condition is true
+
+age = int(input("How old are you? "))
+
+if age == 100:
+    print("You are a century old")
+elif age >= 18:
+    print("You are an adult")
+elif age < 0:
+    print("You have not been born yet")
+else:
+    print("You are a child")
+
+# The order of your if statements matter
+
