8a6675a513cd3069e82e1f678c6fd8321f2ddb32
ToniEngine <antoineorbot@gmail.com>
2024-10-09 02:11:20 +0100

    Updated the Python Learning Folder

---
+++ Python_Learning/11_list/list_constructor.py
@@ -0,0 +1,7 @@
+# Using a list constructor to specify a list
+
+country= list("Nigeria")
+print(country) # Output: ['N', 'i', 'g', 'e', 'r', 'i', 'a']
+
+countries = list(("Nigeria", "Ghana", "Cameroun", "Canada"))
+print(countries)
